8e19201f5501a03b5db270d858ba3bd231b043a5
119kanika <kanika.mahindroo@gmail.com>
2024-04-29 14:10:25 +0530

    gemini

--- src/utils/GeminiAI.jsx
+++ src/utils/GeminiAI.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// import { GEMINI_API } from "./Constant";
+import { GEMINI_API } from "./Constant";
 
-const key="AIzaSyCe5Lb6yVuQnSnjm4mS50q3hXx7G24JZhw"
+const key=GEMINI_API
 console.log(key);
 const genAI = new GoogleGenerativeAI({key})
 
